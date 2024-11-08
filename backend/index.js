ce716abd83f970f9c35901e6e53ca76cb3949719
satya-supercluster <satya.supercluster@gmail.com>
2024-11-09 00:40:09 +0530

    firebase-config and server creation

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// Requires
+const express = require("express"),
+  app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Checking the server
+app.get("/", (req, res) => {
+  res.send("Everything is alright");
+});
+
+
+// Cross Origin Policy
+app.use(cors());
+
+// middlewares
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Listening the server
+app.listen(process.env.PORT, () => {
+  console.log(`I listened something at PORT ${process.env.PORT}`);
+});
